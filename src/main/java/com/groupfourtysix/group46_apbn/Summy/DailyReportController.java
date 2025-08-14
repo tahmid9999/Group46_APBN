1494a628d2a5db7136d85b3eca335e13ed52f696
Half Blood <raihansammy@gmail.com>
2025-08-14 21:41:32 +1000

    Updated FXML added new FXML

---
+++ src/main/java/com/groupfourtysix/group46_apbn/Summy/DailyReportController.java
@@ -0,0 +1,49 @@
+package com.groupfourtysix.group46_apbn.Summy;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class DailyReportController
+{
+    @javafx.fxml.FXML
+    private Button btnClose;
+    @javafx.fxml.FXML
+    private TableColumn DetailsCol;
+    @javafx.fxml.FXML
+    private Label totalBagsLabel;
+    @javafx.fxml.FXML
+    private TableView dailyReportTable;
+    @javafx.fxml.FXML
+    private Label flaggedBagsLabel;
+    @javafx.fxml.FXML
+    private TableColumn totalCol;
+    @javafx.fxml.FXML
+    private Label overweightBagsLabel;
+    @javafx.fxml.FXML
+    private TableColumn flaggedCol;
+    @javafx.fxml.FXML
+    private TableColumn PassengerIDCol;
+    @javafx.fxml.FXML
+    private Label generatedAtLabel;
+    @javafx.fxml.FXML
+    private Button btnExportPdf;
+    @javafx.fxml.FXML
+    private Label totalIncidentsLabel;
+    @javafx.fxml.FXML
+    private TableColumn overweightCol;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void exportToPdf(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void closeWindow(ActionEvent actionEvent) {
+    }
+}
