61710bae79f3fe797cf614a02983379ca0f56930
Half Blood <raihansammy@gmail.com>
2025-08-16 05:23:01 +1000

    Updates

---
+++ src/main/java/com/groupfourtysix/group46_apbn/Summy/ManualCheck.java
@@ -0,0 +1,37 @@
+package com.groupfourtysix.group46_apbn.Summy;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class ManualCheck
+{
+    @javafx.fxml.FXML
+    private Button Backbtn;
+    @javafx.fxml.FXML
+    private TextField FirstBagfield;
+    @javafx.fxml.FXML
+    private TextField PassengerIDField;
+    @javafx.fxml.FXML
+    private TextField secondbagfield;
+    @javafx.fxml.FXML
+    private Button Submitbtn;
+    @javafx.fxml.FXML
+    private Button CheckBagListBtn;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void SubmitSusBaginfo(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void openSusLuggagetable(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void BacktoBaggageHandler(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/com/groupfourtysix/group46_apbn/Summy/OpenReportedPassengerList.java
@@ -0,0 +1,28 @@
+package com.groupfourtysix.group46_apbn.Summy;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class OpenReportedPassengerList
+{
+    @javafx.fxml.FXML
+    private TableColumn colIssuePassengerName;
+    @javafx.fxml.FXML
+    private TableColumn colIssueDescriptipn;
+    @javafx.fxml.FXML
+    private TableView IssueListpassengerTable;
+    @javafx.fxml.FXML
+    private TableColumn colIssueBoardingPass;
+    @javafx.fxml.FXML
+    private Button closeandbackbtn;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void closeandGoback(ActionEvent actionEvent) {
+    }
+}
